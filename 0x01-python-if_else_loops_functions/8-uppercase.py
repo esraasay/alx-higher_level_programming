32c6a7f3b18633e2c2e10556fd392fa2ebd77c8d
esraasay <esraa.krobeen@gmail.com>
2023-11-30 05:39:04 +0300

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,13 @@
-#!usr/bin/python3
-def uppercase(s):
-    for char in s:
-        upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+
+    def uppercase(s):
+        for char in s:
+            upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
         print("{}".format(upper_char), end="")
     print()
