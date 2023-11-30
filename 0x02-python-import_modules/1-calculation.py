7bd597daeb1e9f163ebc9ad0bea16367e5764272
esraasay <esraa.krobeen@gmail.com>
2023-12-01 02:09:39 +0300

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,11 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, subtract, multiply, divide
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
