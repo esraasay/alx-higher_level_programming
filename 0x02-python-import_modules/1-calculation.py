38c0eb88c9708ef237e3849256055f98e489ae0c
esraasay <esraa.krobeen@gmail.com>
2023-12-01 01:56:02 +0300

    done

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
+
+    result_add = add(a, b)
+    result_subtract = subtract(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+
+    print(result_add)
+    print(result_subtract)
+    print(result_multiply)
+    print(result_divide)
