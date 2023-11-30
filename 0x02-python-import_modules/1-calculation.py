a9080deb3fb313e903515e8b60dcc3d3eb968598
esraasay <esraa.krobeen@gmail.com>
2023-12-01 02:07:43 +0300

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
-a = 10
-b = 5
 
 if __name__ == "__main__":
     from calculator_1 import add, subtract, multiply, divide
-
-    result_add = add(a, b)
-    result_subtract = subtract(a, b)
-    result_multiply = multiply(a, b)
-    result_divide = divide(a, b)
-
-    print(result_add)
-    print(result_subtract)
-    print(result_multiply)
-    print(result_divide)
-
-    def add(a, b):
-        return a + b
-
-    def subtract(a, b):
-        return a - b
-
-    def multiply(a, b):
-        return a * b
-
-    def divide(a, b):
-        if b == 0:
-            return "Error: Division by zero"
-        return a / b
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
