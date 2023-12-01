2197006537095165497ce26f5df21291f3f91ca3
esraasay <esraa.krobeen@gmail.com>
2023-12-01 03:42:26 +0300

    doneg

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -12,15 +12,17 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
 
     if operator == '+':
-        result = add(a, b)
+        print("{} + {} = {}".format(a, b, add(a, b)))
+
     elif operator == '-':
-        result = subtract(a, b)
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+
     elif operator == '*':
-        result = multiply(a, b)
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+
     elif operator == '/':
-        result = divide(a, b)
+        print("{} / {} = {}".format(a, b, div(a, b)))
+
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
-    print("{} {} {} = {}".format(a, operator, b, result))
