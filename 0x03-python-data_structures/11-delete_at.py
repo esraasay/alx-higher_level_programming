027aa419d1c79d292e2ed0cadc0d8eaf7a334456
esraasay <esraa.krobeen@gmail.com>
2023-12-05 05:23:29 +0300

    doneg

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-
-    new_list = [element for i, element in enumerate(my_list) if i != idx]
+    new_list = my_list
+    del new_list[idx]
     return new_list
