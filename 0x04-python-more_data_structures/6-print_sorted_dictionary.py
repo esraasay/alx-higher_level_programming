d68ba88400f88d1235a4884bd974f12f2deb815e
esraasay <esraa.krobeen@gmail.com>
2023-12-07 05:19:53 +0300

    doneg

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    return len(a_dictionary)
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
