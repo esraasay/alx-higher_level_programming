b441ede7da8c5b1de26696a7c12a9ff563dd94f1
esraasay <esraa.krobeen@gmail.com>
2023-12-19 04:22:49 +0300

    done

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = 0
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero")
-        return None
-    except (ValueError, TypeError):
-        print("Error: Invalid input types")
-        return None
-    else:
-        print("Inside result: {:.1f}".format(result))
-        return result
+        result = None
     finally:
-        print("Inside finally")
+        print('Inside result: {}'.format(result))
+        return result
