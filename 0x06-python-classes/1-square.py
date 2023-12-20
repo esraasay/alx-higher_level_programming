386751c2c2c5635121758beae7cc449ac002ddd1
esraasay <esraa.krobeen@gmail.com>
2023-12-20 05:18:07 +0300

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+class Square:
+    """Defines a square with a private instance attribute: size"""
+
+    def __init__(self, size):
+        """Instantiates the Square object with a given size."""
+        self.__size = size
