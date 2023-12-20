e5d8dbf3186bdccad40561fb9637cf933b43a1c8
esraasay <esraa.krobeen@gmail.com>
2023-12-20 05:23:28 +0300

    done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""sqare module"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size=0):
+        """constractor.
+
+        args:
+                        size: size of the square
+        raisees:
+            typeerror: type of the square
+            valueerror: value of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
