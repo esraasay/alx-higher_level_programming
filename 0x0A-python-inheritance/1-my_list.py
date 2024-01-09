a0a62245936e8ec7e4a7df1a9ef3c685d9f5ae10
esraasay <esraa.krobeen@gmail.com>
2024-01-09 05:16:34 +0300

    done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""initialize"""
+
+
+class MyList(list):
+    """create mi class mylist with inheritance"""
+
+    def print_sorted(self):
+        """sorted list"""
+        print(sorted(self))
