b5db17337e88743dfa70079bb0fba3abbbf40c20
esraasay <esraa.krobeen@gmail.com>
2024-01-15 21:37:55 +0300

    done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for Base class."""
+class Base:
+    """Base class for project."""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new instance of Base.
+
+        Args:
+            id (int): ID for the new instance.
+
+        Returns:
+            None
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
